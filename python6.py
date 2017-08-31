306d832c0736838f8afb1e9a886f8c9ede1e3641
Joseph <joseph.gunawan97@gmail.com>
2017-08-31 12:22:57 +0700

    Add example

---
+++ python6.py
@@ -0,0 +1,8 @@
+x = int(input("Please enter an integer: "))
+y = int(input("Please enter another integer: "))
+
+if x>y:
+	print(x, "is greather than",y)
+else:
+	print (y,"is greater than or equal to ", x)
+	print("Done.")
